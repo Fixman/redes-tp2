c5b53cd2caf1639428f78ac7a68bfc923453fd78
Martin Fixman <martinfixman@gmail.com>
2014-10-12 22:33:09 -0300

    Agregado Z-score.

--- src/tp2.py
+++ src/tp2.py
@@ -1,12 +1,29 @@
+from numpy import *
 from traceroute import RouteTracer
 
 hosts = [
-    ('Universidade Estadual de Campinas', 'www.unicamp.br'),
-    ('Universita degli study di Milano - Italia', 'www.unimi.it'),
+    ('University of Oxford - Reino Unido', 'www.ox.ac.uk'),
+    ('Universidade Estadual de Campinas - Brasil', 'www.unicamp.br'),
     ('The University of Queensland - Australia', 'uq.edu.au'),
-    ('Massachussets Instutite of Technology - EEUU', 'web.mit.edu'),
+    ('Massachussets Instutite of Technology - Estados Unidos', 'web.mit.edu'),
 ]
 
 for university, host in hosts:
-    print('Traceroute to {} ({}):'.format(university, host))
-    RouteTracer(host).trace_route()
+    distance, rtt_absolute = RouteTracer(host, name=university).trace_route(verbose=True)
+
+    # No estoy contando el primer hop al router porque afea a todos los resultados.
+    # Estoy asumiendo que quiero hacer todos los calculos, incluyendo el promedio y
+    # desvio estandar, sobre las diferencias. Esta bien esto?
+    rtt = diff(rtt_absolute[1:])
+    
+    N = len(rtt)
+    mean = sum(rtt) / N
+    stddev = sqrt(sum([(x - mean) ** 2 for x in rtt]) / N)
+    zscore = [(x - mean) / stddev for x in rtt]
+
+    print('{} {}:'.format(university, host))
+    print('\tmean\t= {}'.format(mean))
+    print('\tstddev\t= {}'.format(stddev))
+    print('\tzscore\t=')
+    for z in zscore:
+        print ('\t\t{}'.format(z))
--- src/traceroute.py
+++ src/traceroute.py
@@ -6,10 +6,11 @@ ICMP_TIME_EXCEEDED = 11
 
 class RouteTracer:
 
-    def __init__(self, dst, times=3, hops=30):
+    def __init__(self, dst, tries=3, hops=30, name=None):
         self.dst    = dst
-        self.times  = times
+        self.tries  = tries
         self.hops   = hops
+        self.name   = name if name else dst
 
     def ip_a_alcanzar(self):
         pkt = scp.IP(dst=self.dst) / scp.ICMP()
@@ -20,15 +21,12 @@ class RouteTracer:
     def nodo_a_distancia(self, ttl):
         #Devuelve la tupla (ip, rtt prom) del nodo a distancia ttl
     
-        apariciones = Counter()
-        tiempo = Counter()
- 
         dst = self.dst
         pkt = scp.IP(dst=dst, ttl=ttl) / scp.ICMP()  
         dst_ip = pkt.dst
 
-        for i in xrange(self.times):
-            ans, unans = scp.sr(pkt, verbose=0, timeout=5)
+        for i in xrange(self.tries):
+            ans, unans = scp.sr(pkt, verbose=0, timeout=1)
 
             if ans:
                 rx = ans[0][1]
@@ -39,26 +37,40 @@ class RouteTracer:
 
         return ('*', 0)
 
-    def trace_route(self):
+    def trace_route(self, verbose=False):
         ip_dst = self.ip_a_alcanzar()
-        print('Trying to reach ip {}.'.format(ip_dst))
 
-        for ttl in xrange(1, self.hops):
+        distance = -1
+        times = []
+        
+        if verbose:
+            print('Traceroute to {} ({}) with IP {}'.format(self.dst, self.name, ip_dst))
+
+        for ttl in xrange(1, self.hops + 1):
             ip, rtt = self.nodo_a_distancia(ttl)
 
             if ip != '*':
-                try:
-                    host = socket.gethostbyaddr(ip)[0]
-                except socket.herror:
-                    host = ip
+                times += [rtt]
 
-                print('{} {} {} {:.3f}'.format(ttl, host, ip, rtt))
-            else:
-                print('{} * * *'.format(ttl))
+            if verbose:
+                if ip != '*':
+                    try:
+                        host = socket.gethostbyaddr(ip)[0]
+                    except socket.herror:
+                        host = ip
+
+                    print('{} {} {} {:.3f}'.format(ttl, host, ip, rtt))
+                else:
+                    print('{} * * *'.format(ttl))
 
             if ip == ip_dst:
-                print('Host reached in {} hops'.format(ttl))
+                distance = ttl
                 break
-        else:
-            print('Host not reached in {} hops'.format(self.hops))
 
+        if verbose:
+            if distance != -1:
+                print('Host reached in {} hops'.format(distance))
+            else:
+                print('Host not reached in {} hops'.format(self.hops))
+
+        return (distance, times)
